222caec49f7f4c1fadddfeee6fe8e39695f79631
janossuta <janossuta86@gmail.com>
2020-10-13 22:14:58 +0200

    init

---
+++ DockerDemoDotnet/Controllers/AppController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerDemoDotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        // GET: api/App
+        [HttpGet("health")]
+        public ActionResult Get()
+        {
+            return Ok("UP");
+        }
+    }
+}
